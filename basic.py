c76ea6aa98860667bbdd63c3822914f97d975f9c
Jasper Ruehl <jasper.ruehl@magellan-net.de>
2025-02-11 16:00:40 +0100

    add warning

--- basic.py
+++ basic.py
@@ -6,4 +6,5 @@ logger = logging.getLogger("mdspo")
 
 logger.debug("hi")
 
-logger.summary_json("this is summary summary")
+logger.summary_json("this is summary")
+logger.warning("something aint right...")
